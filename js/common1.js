1394797637ca490c76200d4541729860e1aaa54f
VitiaVolyk <Vitia.Volyk@gmail.com>
2017-11-29 22:43:40 +0200

    lst

--- js/common1.js
+++ js/common1.js
@@ -21,7 +21,33 @@ $(document).ready(function() {
 		$(this).parent().parent().children(".year").children(".t").stop(false,true).addClass("hidden");
 	});
 	
-	
+	$(".a_1").click(function() {
+		
+		$("body .im_s .a_c").addClass("hidden");
+		$("body .im_s .a_b").addClass("hidden");
+		$("body .wrapp .im_s .a_a").removeClass("hidden");
+		$("body .im_s .a_1").addClass("act");
+		$("body .im_s .a_2").removeClass("act");
+		$("body .im_s .a_3").removeClass("act");
+	});
+	$(".a_2").click(function() {
+		
+		$("body .im_s .a_c").addClass("hidden");
+		$("body .im_s .a_a").addClass("hidden");
+		$("body .wrapp .im_s .a_b").removeClass("hidden");
+		$("body .im_s .a_2").addClass("act");
+		$("body .im_s .a_1").removeClass("act");
+		$("body .im_s .a_3").removeClass("act");
+	});
+	$(".a_3").click(function() {
+		
+		$("body .im_s .a_b").addClass("hidden");
+		$("body .im_s .a_a").addClass("hidden");
+		$("body .wrapp .im_s .a_c").removeClass("hidden");
+		$("body .im_s .a_3").addClass("act");
+		$("body .im_s .a_2").removeClass("act");
+		$("body .im_s .a_1").removeClass("act");
+	});
 
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
